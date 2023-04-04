7c0d2f48b0dc1288045a48737ecfc2b9ed4f5e58
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-04 22:58:33 +0200

    Added students data and refactoring MainViewModel

--- Diary/ViewModels/MainViewModel.cs
+++ Diary/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Diary.Commands;
+using Diary.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +12,63 @@ namespace Diary.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public MainViewModel() 
+        public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+            Students = new ObservableCollection<Student>
+            {
+                new Student
+                {
+                    FirstName="Jakub",
+                    LastName="Zięba",
+                    Group=new Group{Id=1,Name="6B" }
+                },
+                new Student
+                {
+                    FirstName="Jan",
+                    LastName="Zięba",
+                    Group=new Group{Id=2,Name="2B" }
+                },
+                new Student
+                {
+                    FirstName="Zofia",
+                    LastName="Zięba",
+                    Group=new Group{Id=3,Name="1B" }
+                },
+            };
         }
+
         public ICommand RefreshStudentsCommand { get; set; }
+
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
 
 
         private void RefreshStudents(object obj)
         {
-            
+
         }
         private bool CanRefreshStudents(object obj)
         {
