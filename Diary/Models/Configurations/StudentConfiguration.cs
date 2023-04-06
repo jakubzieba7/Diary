06b8838a713e318c24517091bdda43a5d49fd421
Jakub Zięba <jakubzieba7@gmail.com>
2023-04-06 22:14:32 +0200

    Migration setup

--- Diary/Models/Configurations/StudentConfiguration.cs
+++ Diary/Models/Configurations/StudentConfiguration.cs
@@ -9,6 +9,7 @@ namespace Diary.Models.Configurations
         {
             ToTable("dbo.Students");
             HasKey(x => x.Id);
+            Property(x => x.FirstName).HasMaxLength(100).IsRequired();
         }
     }
 }
